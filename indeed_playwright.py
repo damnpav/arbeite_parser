583c03e265cd7397b476e56fca3c26884214b22f
damnpav <hcanilvap@gmail.com>
2023-06-14 22:44:54 +0300

    indeed html saving

--- indeed_playwright.py
+++ indeed_playwright.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
+import time
 
 
 def run(playwright: Playwright) -> None:
@@ -9,6 +10,16 @@ def run(playwright: Playwright) -> None:
     page.get_by_placeholder("Stichwörter, Jobtitel oder Unternehmen").click()
     page.get_by_placeholder("Stichwörter, Jobtitel oder Unternehmen").fill("python")
     page.get_by_role("button", name="Jobs finden").click()
+
+    time.sleep(5)
+
+    page_content = page.content()
+
+    # strategy - download html pages and then parse it out with bs4
+
+    # Save the page content in an HTML file
+    with open("page.html", "w", encoding="utf-8") as file:
+        file.write(page_content)
 
     # ---------------------
     context.close()
