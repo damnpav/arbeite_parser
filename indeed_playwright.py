297ada4bf923b395431ce2db396209477b7aa7e3
damnpav <hcanilvap@gmail.com>
2023-06-16 18:33:49 +0300

    append database handle to main

--- indeed_playwright.py
+++ indeed_playwright.py
@@ -1,5 +1,9 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
+from bs4 import BeautifulSoup as bs
 import time
+import re
+import sqlite3
+from datetime import datetime as dt
 
 
 def run(playwright: Playwright) -> None:
@@ -14,16 +18,31 @@ def run(playwright: Playwright) -> None:
     time.sleep(5)
 
     page_content = page.content()
+    job_id, job_title = parse_html(page_content)
+    write_to_db(job_id, job_title)
 
-    # strategy - download html pages and then parse it out with bs4
-
-    # Save the page content in an HTML file
-    with open("page.html", "w", encoding="utf-8") as file:
-        file.write(page_content)
-
-    # ---------------------
     context.close()
     browser.close()
+
+
+def parse_html(html_content):
+    # %%
+    soup = bs(html_content, 'html.parser')
+    span_tags = soup.find_all('span', attrs={'title': True})
+    job_id = re.findall('jobTitle.*" ', str(span_tags[2]))[0].replace('"', "").replace('jobTitle-', '').replace(' ', '')
+    job_title = re.findall('title=".*">', str(span_tags[2]))[0].replace('title=', '').replace('"', '').replace('>', '')
+    return job_id, job_title
+
+
+def write_to_db(job_id, job_title):
+    conn = sqlite3.connect('mydatabase.db')
+    query = f'''INSERT INTO Jobs (
+                        ID,
+                        JobTitle,
+                        date)
+            VALUES ({job_id}, {job_title}, {dt.now().timestamp()})
+            '''
+    conn.execute(query)
 
 
 with sync_playwright() as playwright:
