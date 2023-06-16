cc4951c52697b48d889c0da5c4f1f964d30cd515
damnpav <hcanilvap@gmail.com>
2023-06-16 18:48:38 +0300

    append database handle to main1

--- indeed_playwright.py
+++ indeed_playwright.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as bs
 import time
 import re
 import sqlite3
+import pandas as pd
 from datetime import datetime as dt
 
 
@@ -18,7 +19,7 @@ def run(playwright: Playwright) -> None:
     time.sleep(5)
 
     page_content = page.content()
-    job_id, job_title = parse_html(page_content)
+    job_ids, job_titles = parse_html(page_content)
     write_to_db(job_id, job_title)
 
     context.close()
@@ -29,20 +30,23 @@ def parse_html(html_content):
     # %%
     soup = bs(html_content, 'html.parser')
     span_tags = soup.find_all('span', attrs={'title': True})
-    job_id = re.findall('jobTitle.*" ', str(span_tags[2]))[0].replace('"', "").replace('jobTitle-', '').replace(' ', '')
-    job_title = re.findall('title=".*">', str(span_tags[2]))[0].replace('title=', '').replace('"', '').replace('>', '')
-    return job_id, job_title
+
+    job_ids = []
+    job_titles = []
+    for span in span_tags:
+        job_ids.append(re.findall('jobTitle.*" ', str(span))[0].replace('"', "").replace('jobTitle-', '').replace(' ', ''))
+        job_titles.append(re.findall('title=".*">', str(span))[0].replace('title=', '').replace('"', '').replace('>', ''))
+    return job_ids, job_titles
 
 
-def write_to_db(job_id, job_title):
+def write_to_db(job_ids, job_titles):
     conn = sqlite3.connect('mydatabase.db')
-    query = f'''INSERT INTO Jobs (
-                        ID,
-                        JobTitle,
-                        date)
-            VALUES ({job_id}, {job_title}, {dt.now().timestamp()})
-            '''
-    conn.execute(query)
+    job_df = pd.DataFrame()
+    job_df['ID'] = job_ids
+    job_df['JobTitle'] = job_titles
+    job_df['date'] = dt.now().strftime('%H:%M:%S %Y-%m-%d')
+    job_df.to_sql('Jobs', conn, if_exists='append')
+    conn.commit()
 
 
 with sync_playwright() as playwright:
